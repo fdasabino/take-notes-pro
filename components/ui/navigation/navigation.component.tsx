9f3b62502cc263659cb20297715dfb099aac6bab
Francisco Sabino <fdasabino@gmail.com>
2025-10-14 18:28:47 +0200

    feat: implement sign-out functionality in NavigationComponent; add dispatch and router hooks for user sign-out

--- components/ui/navigation/navigation.component.tsx
+++ components/ui/navigation/navigation.component.tsx
@@ -1,8 +1,21 @@
+import { ButtonComponent } from "@/components/ui/button/button.component";
 import LogoComponent from "@/components/ui/logo/logo.component";
+import { useAppDispatch } from "@/store/hooks";
+import { signOutUser } from "@/store/thunk/auth.thunk";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import { BiLogOut } from "react-icons/bi";
 
 const NavigationComponent = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    dispatch(signOutUser());
+    router.replace("/auth/login", undefined, { shallow: true });
+  };
+
   return (
     <header className="flex items-center justify-between p-4 border-b border-base_gray mb-4">
       <div>
@@ -14,7 +27,16 @@ const NavigationComponent = () => {
           />
         </Link>
       </div>
-      <nav></nav>
+      <div className="flex items-center gap-4">
+        <nav>
+          <Link href="/stats">Stats</Link>
+        </nav>
+        <button
+          className="text-sm text-red-500 cursor-pointer "
+          onClick={handleSignOut}>
+          <BiLogOut size={20} />
+        </button>
+      </div>
     </header>
   );
 };
