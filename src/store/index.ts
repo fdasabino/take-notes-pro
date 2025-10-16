768bf65f975f4c8bf757ce3e17d98523cd92f8ed
Francisco Sabino <fdasabino@gmail.com>
2025-10-16 11:29:02 +0200

    feat: Enable Redux DevTools in development environment

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/features/auth/state/auth.slice";
 import notesReducer from "@/features/notes/state/notes.slice";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 export const store = configureStore({
+  devTools: isDevelopment,
   reducer: {
     auth: authReducer,
     notes: notesReducer,
